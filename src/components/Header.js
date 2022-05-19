93c18516b4f5a2a36f4dc3ac1be98d93e876764f
Miskevich Stanislav <miskevich95@yandex.ru>
2022-05-19 13:00:53 +0300

    +++

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,26 @@ import logo from "../images/logo-white.svg";
 function Header({ email, onExit }) {
   return (
     <header className="header">
+      <div className="header__container">
       <div className="header__logo-link">
         <img className="header__logo" src={logo} alt="Логотип Место Россия" />
       </div>
-      <Switch>
+      <Switch>     
+        <div className="header__contant">
                     <Route exact path="/">
                         <p className='header__email'>{email}</p>
                         <Link to="/sign-in" className="header__exit" onClick={onExit}>Выйти</Link>
                     </Route>
+                   
                     <Route exact path="/sign-in">
                         <Link to="/sign-up" className="header__link">Регистрация</Link>
                     </Route>
                     <Route exact path="/sign-up">
                         <Link to="/sign-in" className="header__link">Войти</Link>
                     </Route>
+                    </div>
                 </Switch>
+                </div>
     </header>
   );
 }
